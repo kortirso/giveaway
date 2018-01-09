eb0925d245111da2cafa4c1f06f859c7a3514f0e
kortirso <kortirso@gmail.com>
2018-01-09 22:55:03 +0300

    rebuild frontend structure like EvilMartians do

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,9 @@ module Giveaway
             g.test_framework :rspec, fixtures: true, views: false, view_specs: false, helper_specs: false,
                 routing_specs: false, controller_specs: true, request_specs: false
             g.fixture_replacement :factory_bot, dir: 'spec/factories'
+            g.stylesheets false
+            g.javascripts false
+            g.helper false
         end
 
         # Settings in config/environments/* take precedence over those specified here.
