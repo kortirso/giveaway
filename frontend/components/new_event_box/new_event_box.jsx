898962ed2fb9102f66511cc39f59f68b4cb882cd
kortirso <kortirso@gmail.com>
2018-01-10 13:44:10 +0300

    add react component for new event form

---
+++ frontend/components/new_event_box/new_event_box.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class NewEventBox extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+
+  componentWillMount() {
+  }
+
+  render() {
+    return (
+      <main className=''>
+      </main>
+    );
+  }
+}
---
+++ frontend/packs/new_event_box.jsx
@@ -0,0 +1,4 @@
+import NewEventBox from 'components/new_event_box/new_event_box';
+import WebpackerReact from 'webpacker-react';
+
+WebpackerReact.setup({NewEventBox});
