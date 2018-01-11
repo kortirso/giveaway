82f1557041a2120bf9ee5fd9f485d2caaba338a7
kortirso <kortirso@gmail.com>
2018-01-11 10:38:11 +0300

    add privacy, modify frontend components

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,5 +1,6 @@
 import "init";
 import "components/page/page";
 import "components/header/header";
+import "components/footer/footer";
 import "components/welcome/welcome";
 import "components/event_form/event_form";
